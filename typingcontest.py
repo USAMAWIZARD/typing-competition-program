4c35394a11f157d90a2e88fee69591d4cc96a7f1
USAMA <47350008+USAMAWIZARD@users.noreply.github.com>
2019-09-27 01:41:10 +0530

    gui plus logic

--- typingcontest.py
+++ typingcontest.py
@@ -1,6 +1,6 @@
 import csv
 import PySimpleGUI as sg
-
+from pynput.keyboard import Key, Listener
 layout = [ [sg.Text('Name',size=(30, 1),font=("Helvetica", 15), text_color='blue'),sg.InputText(key="name" )],
             [sg.Text('Enrollment no',size=(30, 1), font=("Helvetica", 15), text_color='blue'),sg.InputText(key="enrollmentno" )],
             [sg.Text('type this paragraph',size=(20, 1), font=("Helvetica", 15), text_color='blue'),sg.Text('usama',size=(30, 1), font=("Helvetica", 15), text_color='blue')],
@@ -9,39 +9,55 @@ layout = [ [sg.Text('Name',size=(30, 1),font=("Helvetica", 15), text_color='blue
            [sg.Button('submit' ,key='submit')] ]
 window = sg.Window('Typing Test',location=(0,0),size=(700,800)).Layout(layout).Finalize()
 window.Maximize()
+
+orignal = "i love programming".split()
+appendaword = ""
+i = 0
+error = 0
+
 while True:
-    event,values = window.Read()
+    event, values = window.Read()
+    def appendtostring(key):
+        global appendaword
+        appendaword = str(appendaword + str(key)).replace("'", "")
+
+    def on_press(key):
+        global error, i, appendaword
+        print("hd")
+        if key == Key.space:
+            if appendaword != orignal[i]:
+                error += 1
+                print(error)
+            i += 1
+            appendaword = ""
+
+        try:
+            if (ord(str(key.char)) >= 65 and ord(str(key.char)) <= 90) or (
+                    ord(str(key.char)) >= 97 and ord(str(key.char)) <= 121):
+                appendtostring(key)
+        except:
+            pass
+
+
     if event =='submit':
-        no=sg.PopupGetText("enter passwrod",password_char='*')
-        print(no)
-        if no=="iloveprogramming":
-            name=values['name']
-            enrollmentno = values['enrollmentno']
-            usertyped=values['usertyped']
-            #print(name,enrollmentno,usertyped)
-            orignal="usama is a computerwizard"
-            origanallist = orignal.split(" ")
-            usertypedlist = usertyped.split(" ")
-            orignalindex=0
-            userindex=0
-            i=0
-            print(origanallist)
-            print(usertypedlist)
-            typedwrong=[]
-            for orignal in origanallist:
-                if orignal in usertypedlist:
-                    pass
-                else:
-                    typedwrong.append(orignal)
-            print(typedwrong)
-            datarow=[name,enrollmentno,"eror"]
-            with open('typingdata','a') as csvFile:
-                writer = csv.writer(csvFile)
-                writer.writerow(datarow)
-            csvFile.close()
-            window.FindElement('name').Update('')
-            window.FindElement('enrollmentno').Update('')
-            window.FindElement('name').Update('')
-            window.FindElement('usertyped').Update('')
-        else:
-            sg.PopupError("wrongpassword")
+            no=sg.PopupGetText("enter passwrod",password_char='*')
+            print(no)
+            if no=="iloveprogramming":
+                name=values['name']
+                enrollmentno = values['enrollmentno']
+                usertyped=values['usertyped']
+
+                datarow = [name, enrollmentno,error]
+                with open('typingdata', 'a') as csvFile:
+                    writer = csv.writer(csvFile)
+                    writer.writerow(datarow)
+                csvFile.close()
+
+                window.FindElement('name').Update('')
+                window.FindElement('enrollmentno').Update('')
+                window.FindElement('name').Update('')
+                window.FindElement('usertyped').Update('')
+            else:
+                sg.PopupError("wrongpassword")
+with Listener(on_press=on_press) as listener:
+    listener.join()
